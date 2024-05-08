11849458c0d3c9d3ef51cb2ae977cab03ea0256f
Javier Mosquera <jmosqueralpr@gmail.com>
2024-05-08 13:46:19 -0300

    Actualización: Carga de algunos usuarios

--- src/routes/routesLogin.js
+++ src/routes/routesLogin.js
@@ -11,6 +11,131 @@ const users = [{
     level: 2
 },
 {
+    user: 'bsoria',
+    password: 'bSoria238',
+    level: 2
+},
+{
+    user: 'asolver',
+    password: 'alexS489',
+    level: 2
+},
+{
+    user: 'fscola',
+    password: 'fedeS978',
+    level: 1
+},
+{
+    user: 'jtomatis',
+    password: 'juanT983',
+    level: 1
+},
+{
+    user: 'ecaffa',
+    password: 'enriqueC253',
+    level: 1
+},
+{
+    user: 'rrojas',
+    password: 'ricardoR536',
+    level: 1
+},
+{
+    user: 'wspilere',
+    password: 'willyS369',
+    level: 1
+},
+{
+    user: 'tmoren',
+    password: 'tomasM5215',
+    level: 1
+},
+{
+    user: 'fmarino',
+    password: 'fedeM987',
+    level: 1
+},
+{
+    user: 'gfranconeri',
+    password: 'gianF587',
+    level: 1
+},
+{
+    user: 'ffreire',
+    password: 'ferF512',
+    level: 1
+},
+{
+    user: 'wleguiza',
+    password: 'walterL',
+    level: 1
+},
+{
+    user: 'mpardo',
+    password: 'manuelP995',
+    level: 1
+},
+{
+    user: 'ldlpeña',
+    password: 'luisdlP563',
+    level: 1
+},
+{
+    user: 'fsecul',
+    password: 'fabioS563',
+    level: 1
+},
+{
+    user: 'cscrofani',
+    password: 'christianS325',
+    level: 1
+},
+{
+    user: 'mescobar',
+    password: 'mairaE278',
+    level: 1
+},
+{
+    user: 'balizador573',
+    password: 'bal573',
+    level: 1
+},
+{
+    user: 'balizador574',
+    password: 'bal574',
+    level: 1
+},
+{
+    user: 'balizadorBoyero',
+    password: 'balBoyero',
+    level: 1
+},
+{
+    user: 'balizadorMula',
+    password: 'balMula',
+    level: 1
+},
+{
+    user: 'lanchaCharrua',
+    password: 'lanChar',
+    level: 1
+},
+{
+    user: 'lanchaDonOsvaldo',
+    password: 'lanDonOsv',
+    level: 1
+},
+{
+    user: 'lanchaKawai',
+    password: 'lanKawai',
+    level: 1
+},
+{
+    user: 'admin',
+    password: 'administrador',
+    level: 1
+},
+{
     user: 'admin',
     password: 'administrador',
     level: 1
